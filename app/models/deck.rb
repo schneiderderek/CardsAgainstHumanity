05095a8eb9c107a1f7663a2d84ad78cac1bea016
schneiderderek <schneidermd1993@gmail.com>
2013-11-05 14:58:26 -0500

    Fixed some associations

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,6 @@
 class Deck < ActiveRecord::Base
-  attr_accessible :name, :num_black_cards, :num_white_cards, :user, :game
+  attr_accessible :name, :num_black_cards, 
+    :num_white_cards, :user, :game
 
   belongs_to :user
   belongs_to :game # if game_id is nil then the deck is not being played
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,9 @@
 class Game < ActiveRecord::Base
-  attr_accessible :players, :name, :max_players, :finished, :deck
+  attr_accessible :players, :name, :max_players, :finished, 
+    :deck, :users, :hands
   
-  has_many :users
   has_one :deck
+  has_many :users
   has_many :hands, through: :users
 
   validates :name, :max_players, :deck, presence: true
