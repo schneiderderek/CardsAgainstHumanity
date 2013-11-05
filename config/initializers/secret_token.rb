4f0e8275f4c5ab9bebc2b444ae943ea5898ed781
schneiderderek <schneidermd1993@gmail.com>
2013-11-04 19:19:26 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardsAgainstHumanity::Application.config.secret_token = '817d4882fa7962045de22101f5a2d171ad112e6488e61159d7421c416698cb9beaa12b8cc9862ae43e0e59e1dd218739daddf264702299405ed9280c016c4cb9'
