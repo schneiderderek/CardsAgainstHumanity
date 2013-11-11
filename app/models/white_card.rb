b7cfd13a6826e22a96526ba8cd0eaf38e6018f91
schneiderderek <schneidermd1993@gmail.com>
2013-11-11 15:19:03 -0500

    Added validation for white card model

--- app/models/white_card.rb
+++ app/models/white_card.rb
@@ -5,11 +5,17 @@ class WhiteCard < ActiveRecord::Base
   belongs_to :hand
 
   validates :content, :deck, presence: true
+  validate :deck_or_hand
 
   def duplicate_for(new_deck)
     white_card = self.dup
     white_card.deck = new_deck
     white_card.hand = nil
     white_card.save
+  end
+
+  private
+  def deck_or_hand
+    errors.add "White card cannot be assigned to a hand and deck" if self.hand && self.deck
   end
 end
