60120501a2c0c69c7190819ccddf122fe1860e78
schneiderderek <schneidermd1993@gmail.com>
2014-02-15 23:00:18 -0500

    Maybe this will work better

--- db/migrate/20131106063454_remove_card_counts_from_deck.rb
+++ db/migrate/20131106063454_remove_card_counts_from_deck.rb
@@ -1,6 +1,8 @@
 class RemoveCardCountsFromDeck < ActiveRecord::Migration
   def change
-    remove_column :decks, :num_white_cards
-    remove_column :decks, :num_black_cards
+    change_table :decks do |t|
+      t.remove :num_white_cards
+      t.remove :num_black_cards
+    end
   end
 end
